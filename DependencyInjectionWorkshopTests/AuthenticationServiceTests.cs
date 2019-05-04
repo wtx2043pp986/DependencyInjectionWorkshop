5c504089b17eff8b2b0a9e54e0a0393ab9a34e21
Roberson <wtx2043pp986@gmail.com>
2019-05-04 15:51:02 +0800

    refactor test first

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,8 @@
-﻿using DependencyInjectionWorkshop.Models;
+﻿using DependencyInjectionWorkshop.Adapters.Interfaces;
+using DependencyInjectionWorkshop.Models;
+using DependencyInjectionWorkshop.Models.Interfaces;
+using DependencyInjectionWorkshop.Repository;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -6,10 +10,68 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private const string DefaultAccountId = "roberson";
+        private const string DefaultHashPassword = "my_hash_password";
+        private const string DefaultPassword = "password";
+        private const string DefautOtp = "123456";
+        private IProfile _profile;
+        private IOtp _otpRemoteProxy;
+        private IHash _hash;
+        private INotification _notification;
+        private IFailedCounter _failedCounter;
+        private ILogger _logger;
+        private AuthenticationService _authenticationService;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Use NSubstitute to mock objects.
+            _profile = Substitute.For<IProfile>();
+            _otpRemoteProxy = Substitute.For<IOtp>();
+            _hash = Substitute.For<IHash>();
+            _notification = Substitute.For<INotification>();
+            _failedCounter = Substitute.For<IFailedCounter>();
+            _logger = Substitute.For<ILogger>();
+            _authenticationService =
+                new AuthenticationService(_failedCounter, _profile, _hash, _otpRemoteProxy, _logger, _notification);
+        }
+
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+
+            GivenPassword(DefaultAccountId, DefaultHashPassword);
+            GivenHash(DefaultHashPassword, DefaultPassword);
+            GivenOtp(DefaultAccountId, DefautOtp);
+
+            var verify = WhenVerify(DefaultAccountId, DefaultPassword, DefautOtp);
+            
+            ShouldBeValid(verify);
+        }
+
+        private static void ShouldBeValid(bool verify)
+        {
+            Assert.IsTrue(verify);
+        }
+
+        private void GivenOtp(string accountId, string otp)
+        {
+            _otpRemoteProxy.GetCurrentOtp(accountId).ReturnsForAnyArgs(otp);
+        }
+
+        private void GivenHash(string hashPassword, string password)
+        {
+            _hash.GetHash(password).ReturnsForAnyArgs(hashPassword);
+        }
+
+        private void GivenPassword(string accountId, string hashPassword)
+        {
+            _profile.GetPassword(accountId).ReturnsForAnyArgs(hashPassword);
+        }
+
+        private bool WhenVerify(string accountId, string password, string otp)
+        {
+            return _authenticationService.Verify(accountId, password, otp);
         }
     }
 }
