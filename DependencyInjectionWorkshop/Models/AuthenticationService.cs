c6415d3811e0cf9cc9823ad48d26aa1c64bd526c
Roberson <wtx2043pp986@gmail.com>
2019-05-04 09:50:19 +0800

    Add Verify Feature to AuthenticationService

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,54 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+
+        public bool Verify(string accountId, string password, string otp)
+        {
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                var hashedPasswordFromDb = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+
+                var crypt = new System.Security.Cryptography.SHA256Managed();
+                var hash = new StringBuilder();
+                var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+                foreach (var theByte in crypto)
+                    hash.Append(theByte.ToString("x2"));
+
+                var hashedPassword = hash.ToString();
+                
+                var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+                var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+
+                string currentOtp;
+                if (response.IsSuccessStatusCode)
+                {
+                    currentOtp = response.Content.ReadAsAsync<string>().Result;
+                }
+                else
+                {
+                    throw new Exception($"web api error, accountId:{accountId}");
+                }
+
+                if (hashedPasswordFromDb == hashedPassword && currentOtp == otp)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
     }
 }
